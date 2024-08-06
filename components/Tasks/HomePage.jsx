dcce99acbd6d1342652a32e2ecce6f8ae9ff3217
kashmala mahsud <youshaykhan567>
2024-08-06 11:34:04 -0700

    NFT Cards

--- components/Tasks/HomePage.jsx
+++ components/Tasks/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -12,8 +12,9 @@ const apiUrl = 'http://localhost:4000/nfts';
 function HomePage() {
   const [show, setShow] = useState(false);
   const [nfts, setNfts] = useState([]);
+  const [filteredNfts, setFilteredNfts] = useState([]);
   const [formValues, setFormValues] = useState({
-    image: '',
+    image: null,
     title: '',
     rank: '',
     author: '',
@@ -26,11 +27,13 @@ function HomePage() {
   const [editingNft, setEditingNft] = useState(null);
   const [toastMessage, setToastMessage] = useState(null);
   const [modalLoading, setModalLoading] = useState(false);
-
+  const [searchNfts, setSearchNfts] = useState('');
+  const [imagePreview, setImagePreview]= useState(null);
   useEffect(() => {
     axios.get(apiUrl)
       .then(response => {
         setNfts(response.data);
+        setFilteredNfts(response.data);
         setLoading(false);
       })
       .catch(error => {
@@ -39,44 +42,48 @@ function HomePage() {
       });
   }, []);
 
-  const resetState = () => {
+  const handleClose = () => {
+    setShow(false);
     setFormValues({
-      image: '',
+      image: null,
       title: '',
       rank: '',
       author: '',
       price_eth: '',
       price_usd: ''
     });
+    setImagePreview(null); //clear image preview
     setErrors({});
     setEditingNft(null);
-    setModalLoading(false);
-    setShow(false);
   };
-
-  const handleClose = () => resetState();
 
   const handleShow = () => setShow(true);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
+    const { name, value, files } = e.target;
+    if (name === "image" && files.length > 0) {
+      setFormValues({ ...formValues, image: files[0] });
+      //create a URL for the selected  image file
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setFormValues({ ...formValues, image: reader.result });
-      };
-      reader.readAsDataURL(file);
+      const previewUrl= URL.createObjectURL(files[0]);
+      setImagePreview(previewUrl)
+      // OR
+      //const reader= new FileReader();
+      // reader.onloadend=()=>{
+      //   setImagePreview(reader.result);
+      // }
+      // reader.readAsDataURL(files[0]);
+      
+
+
+    } else {
+      setFormValues({ ...formValues, [name]: value });
     }
   };
 
   const validateForm = () => {
     let formErrors = {};
-    if (!formValues.image) formErrors.image = "Image is required";
+    if (!formValues.image && !editingNft) formErrors.image = "Image is required";
     if (!formValues.title) formErrors.title = "Title is required";
     if (!formValues.rank) formErrors.rank = "Rank is required";
     if (!formValues.author) formErrors.author = "Author is required";
@@ -85,47 +92,91 @@ function HomePage() {
     return formErrors;
   };
 
+  const resetState = () => {
+    setModalLoading(false);
+    handleClose();
+  };
+
   const handleSaveChanges = () => {
     const formErrors = validateForm();
     if (Object.keys(formErrors).length === 0) {
       setModalLoading(true);
-      if (editingNft) {
-        axios.put(`${apiUrl}/${editingNft.id}`, formValues)
-          .then(response => {
-            const updatedNfts = nfts.map(nft => nft.id === editingNft.id ? response.data : nft);
-            setNfts(updatedNfts);
-            setToastMessage('NFT updated successfully!');
-          })
-          .catch(error => {
-            setToastMessage('Error updating NFT');
-          })
-          .finally(() => resetState());
-      } else {
-        axios.post(apiUrl, formValues)
-          .then(response => {
-            setNfts([...nfts, response.data]);
-            setToastMessage('NFT added successfully!');
-          })
-          .catch(error => {
-            setToastMessage('Error adding NFT');
-          })
-          .finally(() => resetState());
+  
+      const formData = new FormData();
+      formData.append('title', formValues.title);
+      formData.append('rank', formValues.rank);
+      formData.append('author', formValues.author);
+      formData.append('price_eth', formValues.price_eth);
+      formData.append('price_usd', formValues.price_usd);
+      if(formValues.image){
+        formData.append('image', formValues.image);
       }
-    } else {
-      setErrors(formErrors);
+      const axiosConfig={
+        headers:{
+          "content-Type":"application/json"
+        }
+      }
+  
+      console.log('Form Data:');
+      for (let pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+      }
+
+  
+      if(editingNft){
+        axios.put(`${apiUrl}/${editingNft.id}`, formData, axiosConfig
+        ) 
+        .then(response => {
+          const updatedNft={
+            ...response.data, image: URL.createObjectURL(formValues.image)
+          }
+            const updatedNfts =  nfts.map(nft=>nft.id === editingNft.id ? updatedNft : nft)
+            setNfts(updatedNfts);
+            setFilteredNfts(updatedNfts);
+            setToastMessage('NFT updated successfully!'); 
+          })
+          .catch(error => {
+              setToastMessage("Error updating NFT");
+            })
+            
+          .finally(() => {
+            resetState();
+          });
+      } else {
+      axios.post(apiUrl, formData, axiosConfig)
+      .then((response) => {
+        const newNft= {
+          ...response.data, image:URL.createObjectURL(formValues.image)
+        }
+        setNfts([...nfts, newNft]);
+        setFilteredNfts([...nfts, newNft])
+        setToastMessage("NFT added successfully");
+      }).catch((err) => {
+        setToastMessage("Error adding NFT")      }).finally(()=>{
+          resetState()
+        })
     }
-  };
+  }
+  else{
+    setErrors(formErrors);
+  }};
 
   const handleEdit = (nft) => {
     setEditingNft(nft);
-    setFormValues(nft);
+    setImagePreview(nft.image); // Set the preview to the existing image URL
+    setFormValues({
+      ...nft,
+      image: null // Reset image to avoid re-upload
+    });
     setShow(true);
   };
 
   const handleDelete = (id) => {
     axios.delete(`${apiUrl}/${id}`)
       .then(() => {
-        setNfts(nfts.filter(nft => nft.id !== id));
+        const updatedNfts = nfts.filter(nft => nft.id !== id);
+        setNfts(updatedNfts);
+        setFilteredNfts(updatedNfts);
         setToastMessage('NFT deleted successfully!');
       })
       .catch(error => {
@@ -133,12 +184,48 @@ function HomePage() {
       });
   };
 
+  const handleSearchChange = (e) => {
+    setSearchNfts(e.target.value);
+    debounceFilterNfts(e.target.value);
+  };
+
+  const debounce = (func, delay) => {
+    let debounceTimer;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+  const filterNfts = (term) => {
+    if (term) {
+      const filtered = nfts.filter(nft =>
+        nft.title.toLowerCase().includes(term.toLowerCase()) ||
+        nft.rank.toLowerCase().includes(term.toLowerCase()) ||
+        nft.author.toLowerCase().includes(term.toLowerCase())
+      );
+      setFilteredNfts(filtered);
+    } else {
+      setFilteredNfts(nfts);
+    }
+  };
+
+  const debounceFilterNfts = useCallback(debounce(filterNfts, 50), [nfts]);
+
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        + Add/Edit NFT
+      <Button variant="success" onClick={handleShow}>
+        + Add / Edit NFT
       </Button>
-
+      <Form.Control
+        type='text'
+        placeholder='Search NFTs'
+        onChange={handleSearchChange}
+        value={searchNfts}
+        className='mt-3'
+      />
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{editingNft ? 'Edit NFT' : 'Add NFT'}</Modal.Title>
@@ -150,12 +237,17 @@ function HomePage() {
               <Form.Control
                 type="file"
                 accept="image/*"
-                onChange={handleFileChange}
+                name="image"
+                onChange={handleChange}
                 isInvalid={!!errors.image}
               />
               <Form.Control.Feedback type="invalid">
                 {errors.image}
               </Form.Control.Feedback>
+              {imagePreview && (<img 
+              src={imagePreview}
+            alt="imagePreview"
+          style={{width:"100%", marginTop:"10px"}}/>)}
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Title</Form.Label>
@@ -227,7 +319,9 @@ function HomePage() {
                 {errors.price_usd}
               </Form.Control.Feedback>
             </Form.Group>
-            {modalLoading && <Spinner animation="border" />}
+            {modalLoading && (
+              <Spinner animation="border" />
+            )}
           </Form>
         </Modal.Body>
         <Modal.Footer>
@@ -235,28 +329,10 @@ function HomePage() {
             Close
           </Button>
           <Button variant="primary" onClick={handleSaveChanges}>
-            Save Changes
+            {editingNft ? 'Save Changes' : 'Add NFT'}
           </Button>
         </Modal.Footer>
       </Modal>
-
-      {loading ? (
-        <Spinner animation="border" />
-      ) : fetchError ? (
-        <div>{fetchError}</div>
-      ) : (
-        <div className="nft-cards-container">
-          {nfts.map((nft) => (
-            <NftCards
-              key={nft.id}
-              nft={nft}
-              onEdit={handleEdit}
-              onDelete={handleDelete}
-            />
-          ))}
-        </div>
-      )}
-
       {toastMessage && (
         <Toast
           onClose={() => setToastMessage(null)}
@@ -271,7 +347,24 @@ function HomePage() {
           <Toast.Body>{toastMessage}</Toast.Body>
         </Toast>
       )}
+      {loading ? (
+        <Spinner animation="border" />
+      ) : fetchError ? (
+        <div>{fetchError}</div>
+      ) : (
+        <div className="nft-cards-container">
+          {filteredNfts.map((nft) => (
+            <NftCards
+              key={nft.id}
+              nft={nft}
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 }
+
 export default HomePage;
--- components/Tasks/NftsCards.jsx
+++ components/Tasks/NftsCards.jsx
@@ -8,15 +8,15 @@ function NftCards({ nft , onEdit , onDelete}) {
     <div style={{ width:"50%" , height:"auto"}}>
     <div className="card" >
       <div className='card-body'>
-      <img src={nft.image} alt={nft.title} style={{width:"100px" , height:"100px"}}/>
+      <img src={nft.image} alt={nft.title} style={{width:"250px" , height:"250px"}}/>
       <h3>{nft.title}</h3>
       <p>Rank: {nft.rank}</p>
       <p>Author: {nft.author}</p>
       <p>Price (ETH): {nft.price_eth}</p>
       <p>Price (USD): {nft.price_usd}</p>
       <div className='button-group'>
-      <button variant="primary" onClick={()=> onEdit(nft)}>Edit</button>
-      <button variant="danger" onClick={()=> onDelete(nft.id)}>Delete</button></div>
+      <button variant="primary" style={{backgroundColor:"lightblue",margin:"10px", color:"white"}} onClick={()=> onEdit(nft)}>Edit</button>
+      <button variant="danger" style={{backgroundColor:"red", color:"white"}} onClick={()=> onDelete(nft.id)}>Delete</button></div>
     </div></div></div>
   );
 }
